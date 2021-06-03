ace52f56f53891117219f1877db0c2ca4e48652c
jumpx2 <jumpx2@icloud.com>
2021-06-03 22:53:59 +0900

    vue3

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1> {{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2
+    }
+  },
+  beforeCreate() {
+    console.log('Before Create!')
+  },
+  created() {
+    console.log('Created', this.count)
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('Mounted!')
+    console.log(document.querySelector('h1'))
+  }
+}
+</script>
